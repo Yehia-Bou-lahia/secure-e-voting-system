a42e1b66e00c9614b5583016b1b91f80bdb32781
Yehia Boulahia <yehia.boulahia@protonmail.com>
2026-04-27 19:47:07 +0100

    fix(asgi): reorder imports to avoid AppRegistryNotReady error

--- encryption_project/asgi.py
+++ encryption_project/asgi.py
@@ -1,11 +1,18 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from voting.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'encryption_project.settings')
 
+django_asgi_app = get_asgi_application()
+
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from voting.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": URLRouter(websocket_urlpatterns),
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })
