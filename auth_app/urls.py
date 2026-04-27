35d6c35153e615c1dbd2c1fc85db18aa8078c09b
Yehia Boulahia <yehia.boulahia@protonmail.com>
2026-04-27 19:46:58 +0100

    feat(urls): add results endpoint and cleanup routes

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import UserView, RegisterView, StudentViewSet, CandidateViewSet
+from .views import results
 
 router = DefaultRouter()
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/results/', results, name='results'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('self', UserView.as_view(), name='user_self_details'),
     path('register/', RegisterView.as_view(), name='user_register')]
